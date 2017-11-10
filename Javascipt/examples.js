e41464f51aa64a4cbbc77e1ab2bc310a96c7409c
k90armstrong <k90armstrong@gmail.com>
2017-11-10 14:37:20 -0700

    adds clone method example

---
+++ Javascipt/examples.js
@@ -0,0 +1,4 @@
+// cloneNode(bool if deep "all children and attributes" true if is deep)
+// example usage cloning a card with all of it's children
+var card = document.getElementById('#card');
+var clonedCard = card.cloneNode(true);
